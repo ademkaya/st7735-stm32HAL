3800ce39dea6974d59f877d1c21d8eb5bed6ca66
adem <swer.rock@gmail.com>
2022-03-07 20:16:31 +0300

    converter app is added
    small fix

--- st7735_img2arr/Form1.Designer.cs
+++ st7735_img2arr/Form1.Designer.cs
@@ -70,9 +70,9 @@ namespace st7735_img2arr
             this.label1.AutoSize = true;
             this.label1.Location = new System.Drawing.Point(259, 12);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(46, 17);
+            this.label1.Size = new System.Drawing.Size(93, 17);
             this.label1.TabIndex = 3;
-            this.label1.Text = "label1";
+            this.label1.Text = "width x height";
             // 
             // btn_import
             // 
